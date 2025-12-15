d08f29a0bf926949878a375c0345e0ffaee17d11
mini-grossberg <aadhyanadig.n@gmail.com>
2025-12-15 10:39:13 +0000

    Movement and Food
    The snake moves and eats food. If it eats the food - it grows.

--- src/snek/Game.java
+++ src/snek/Game.java
@@ -3,10 +3,12 @@ package snek;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.Random;
 
-class GamePanel extends JPanel {
+class GamePanel extends JPanel implements ActionListener{
 	final Color PANEL = new Color(0x383b3d);
 	final Color SNAKE = new Color(0x84bd91);
+	final Color FOOD = new Color(0xe3496f);
 	
 	final int PANELSIZE = 900; 								// Size of the total playable area in pixels.
 	final int SNAKEPIXEL = 20; 								// Size of the snake in pixels.
@@ -17,6 +19,23 @@ class GamePanel extends JPanel {
 	final int[] y = new int[MAX_SNAKE_LENGTH];				// Y Coordinates of the Snake.
 	
 	int snakeLength = 5;									// Initial Length of the Snake.
+	
+	static final int UP = -2;								// Direction -- UP
+	static final int DOWN = 2;								// Direction -- DOWN
+	static final int RIGHT = 1;								// Direction -- RIGHT
+	static final int LEFT = -1;								// Direction -- LEFT
+	
+	private final Random random = new Random();
+	private int foodX;
+	private int foodY;
+	
+	boolean first = true;
+	
+	Timer timer;
+	int DELAY = 60;
+	
+	static int direction = RIGHT;
+	boolean running = false;
 	
 	public GamePanel() {
 		this.setBackground(PANEL);
@@ -30,12 +49,69 @@ class GamePanel extends JPanel {
 			x[i] = GRIDPIXEL/2 - i;
 			y[i] = GRIDPIXEL/2;
 		}
+		
+		this.addKeyListener(new KeyPressed());
+		this.setFocusable(true);
+		this.requestFocusInWindow(true);
+		
+		startGame();
 	}
 	
+	public void startGame() {
+		initFood();
+		running = true;
+		timer = new Timer(DELAY, this);
+		timer.start();
+	}
+	
+	public void initFood() {
+		foodX = random.nextInt(GRIDPIXEL-2);
+		foodY = random.nextInt(GRIDPIXEL-2);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (running) {
+			move();
+			checkFood();
+		}
+		
+		repaint();
+	}
+	
+	public void move() {
+		for (int i = snakeLength; i > 0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
+		}
+		
+		switch(direction) {
+		case RIGHT:
+			x[0] = x[0] + 1;
+			break;
+		case LEFT:
+			x[0] = x[0] - 1;
+			break;
+		case UP:
+			y[0] = y[0] - 1;
+			break;
+		case DOWN:
+			y[0] = y[0] + 1;
+			break;
+		}
+	}
+	
+	public void checkFood() {
+		if ((x[0] == foodX && y[0] == foodY)) {
+			snakeLength++;
+			initFood();
+		}
+	}
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		drawGrid(g);
+		spawnFood(g);
 		drawSnake(g);
 	}
 
@@ -57,6 +133,36 @@ class GamePanel extends JPanel {
 		
 		for (int i = 0; i < snakeLength; i++)	{
 			g.fillRect(x[i] * SNAKEPIXEL, y[i] * SNAKEPIXEL, SNAKEPIXEL, SNAKEPIXEL);
+		}
+	}
+	
+	public void spawnFood(Graphics g) {
+		g.setColor(FOOD);
+			
+		g.fillRect(foodX * SNAKEPIXEL, foodY * SNAKEPIXEL, SNAKEPIXEL, SNAKEPIXEL);
+	}
+}
+
+class KeyPressed extends KeyAdapter {
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_A:
+			if (GamePanel.direction != GamePanel.RIGHT) { 
+				GamePanel.direction = GamePanel.LEFT; 
+			}break; 
+		case KeyEvent.VK_D:
+			if (GamePanel.direction != GamePanel.LEFT) { 
+				GamePanel.direction = GamePanel.RIGHT; 
+			}break; 
+		case KeyEvent.VK_W:
+			if (GamePanel.direction != GamePanel.DOWN) { 
+				GamePanel.direction = GamePanel.UP; 
+			}break; 
+		case KeyEvent.VK_S:
+			if (GamePanel.direction != GamePanel.UP) { 
+				GamePanel.direction = GamePanel.DOWN; 
+			}break; 
 		}
 	}
 }
